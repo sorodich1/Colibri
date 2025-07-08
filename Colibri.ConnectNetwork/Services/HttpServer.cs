a86eedc4e3e8fae7ec67182393ed79e47d83fe0b
sorodich1 <sorodich@gmail.com>
2025-07-08 14:56:13 +0300

    Реализация библиотеки работающей с протоколами обмена данными.

---
+++ Colibri.ConnectNetwork/Services/HttpServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ConnectNetwork.Services
+{
+    public class HttpServer
+    {
+        private readonly HttpListener _listener = new ();
+
+        public void Start(string prefic)
+        {
+            _listener.Prefixes.Add(prefic);
+            _listener.Start();
+
+            Console.WriteLine("Сервер запущен...");
+
+            Task.Run(() => AcceptClient());
+        }
+
+        private async Task AcceptClient()
+        {
+            while (true)
+            {
+                var context = await _listener.GetContextAsync();
+                _ = Task.Run(() => HandleRequest(context));
+            }
+        }
+
+        private async Task HandleRequest(HttpListenerContext context)
+        {
+            Console.WriteLine("Подключение клиента...");
+
+            string responseString = string.Empty;
+
+            switch (context.Request.HttpMethod)
+            {
+                case "GET":
+                    responseString = "Поступил GET запрос";
+                    break;
+                case "POST":
+                    using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                    {
+                        responseString = await reader.ReadToEndAsync();
+                        Console.WriteLine("POST запрос: " + responseString);
+                    }
+                    break;
+                case "PUT":
+                    using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                    {
+                        responseString = await reader.ReadToEndAsync();
+                        Console.WriteLine("PUT запрос: " + responseString);
+                    }
+                    break;
+                case "DELETE":
+                    responseString = "Ресурс удалён";
+                    Console.WriteLine("Поступил DELETE запрос");
+                    break;
+                default:
+                    responseString = "Метод не найден";
+                    context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                    break;
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            context.Response.ContentLength64 = buffer.Length;
+            await context.Response.OutputStream.WriteAsync(buffer);
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            context.Response.Close();
+            Console.WriteLine("Ответ отправлен");
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+        }
+    }
+}
---
+++ Colibri.ConnectNetwork/Services/TcpServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ConnectNetwork.Services
+{
+    public class TcpServer
+    {
+        private TcpListener _listener;
+
+        public void Start(int port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+            _listener.Start();
+            Console.WriteLine("Server is listener... ");
+
+            Task.Run(() => AcceptClients());
+        }
+
+        private async Task AcceptClients()
+        {
+            while (true)
+            {
+                var client = await _listener.AcceptTcpClientAsync();
+                await Task.Run(() => HandleClient(client));
+            }
+        }
+
+        private async Task HandleClient(TcpClient client)
+        {
+            Console.WriteLine("Клиент подключен.");
+
+            var stream = client.GetStream();
+
+            while (client.Connected)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0) break;
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine("Ответ от сервера: " + message);
+
+                await stream.WriteAsync(buffer.AsMemory(0, bytesRead));
+            }
+            client.Close();
+            Console.WriteLine("Соединение закрыто.");
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+        }
+    }
+}
