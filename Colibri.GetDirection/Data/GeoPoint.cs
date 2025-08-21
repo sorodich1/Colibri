95eb3e450ba83dad24affe95fadfeb8a8ad0baf0
sorodich1 <sorodich@gmail.com>
2025-08-21 20:59:44 +0300

    Полётное задание, десириализация.

---
+++ Colibri.GetDirection/Data/GeoPoint.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Colibri.GetDirection.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GeoPoint
+    {
+        public double Latitude {  get; set; }
+        public double Longitude {  get; set; }
+    }
+
+    //public class Poligon
+    //{
+    //    public bool Inclusion { get; set; }
+    //    public List<List<GeoPoint>> Polygons {  get; set; }
+    //    public int Version {  get; set; }
+    //}
+
+    public class MissionItem
+    {
+        public object AMSLAltAboveTerrain {  get; set; }
+        public double Altitude {  get; set; }
+        public int AltitudeMode {  get; set; }
+        public bool AutoContine {  get; set; }
+        public int Command { get; set; }
+        public int DoJumpId { get; set; }
+        public int Frame { get; set; }
+        public List<object> Params {  get; set; }
+        public string Type {  get; set; }
+    }
+
+    public class Mission
+    {
+        public double CruiseSpeed { get; set; }
+        public int FirmwareType { get; set; }
+        public int GlobalPlanAltitudeMode { get; set; }
+        public double HoverSpeed { get; set; }
+        public List<MissionItem> Items { get; set; }
+        public List<double> PlannedHomePosition { get; set; }
+        public int VehicleType { get; set; }
+        public int Version { get; set; }
+    }
+
+    public class PlanModel
+    {
+        public string FileType { get; set; }
+        public string GroundStation {  get; set; }
+        public Mission Mission {  get; set; }
+        public int Version { get; set; }
+    }
+}
--- Colibri.GetDirection/DirectionJson.cs
+++ Colibri.GetDirection/DirectionJson.cs
@@ -1,16 +1,28 @@
 ﻿using Colibri.GetDirection.Helpers;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 using System;
+using Colibri.GetDirection.Data;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Colibri.GetDirection
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class DirectionJson
     {
-        public static string RouteFly(Point start, Point stop)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="stop"></param>
+        /// <returns></returns>
+        public static Task<GeodistanceRequest> RouteFly(Point start, Point stop)
         {
             var distance = Math.Round(ProcessRequest.CalculateDistance(start, stop), 3);
 
-            var distanceList = ProcessRequest.GenerateRoutePoints(start, stop, 100);
+            var distanceList = ProcessRequest.GenerateRoutePoints(start, stop, 10000);
 
             GeodistanceRequest geodistanceRequest = new()
             {
@@ -21,7 +33,79 @@ namespace Colibri.GetDirection
                 Route = "По прямой"
             };
 
-            return JsonConvert.SerializeObject(geodistanceRequest);
+            return Task.FromResult(geodistanceRequest);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static Task<string> MissionFile(GeodistanceRequest model)
+        {
+            List<MissionItem> missions = [];
+
+            int index = 0;
+
+            foreach (var item in model.RoutePoints)
+            {
+                index++;
+
+                int command = 16;
+
+                if (index == 1)
+                    command = 22;
+                else if (index == model.RoutePoints.Count)
+                    command = 21;
+
+                MissionItem missionItem = new()
+                {
+                    Altitude = item.Latitude,
+                    Params =
+                    [
+                        0,
+                        0,
+                        0,
+                        null,
+                        item.Latitude,
+                        item.Longitude,
+                        50
+                    ],
+                    AltitudeMode = 1,
+                    AMSLAltAboveTerrain = null,
+                    AutoContine = true,
+                    Command = command,
+                    DoJumpId = index,
+                    Frame = 3,
+                    Type = "SimpleItem"
+                };
+
+                missions.Add(missionItem);
+            }
+
+            Mission mission = new()
+            {
+                CruiseSpeed = 15,
+                FirmwareType = 3,
+                GlobalPlanAltitudeMode = 1,
+                HoverSpeed = 5,
+                Items = missions,
+                PlannedHomePosition = [],
+                VehicleType = 2,
+                Version = 2
+            };
+
+            PlanModel planModel = new()
+            {
+                FileType = "MissionPlan",
+                GroundStation = "QGroundControl",
+                Version = 1,
+                Mission = mission
+            };
+
+            string str = JsonConvert.SerializeObject(planModel, Formatting.Indented);
+
+            return Task.FromResult(str);
         }
     }
 }
